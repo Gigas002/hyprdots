a10315868a9e27c53743ae2eb16ea0c25d939551
gigas002 <gigas002@pm.me>
2024-02-29 22:36:00 +0900

    Minor fixes

--- Configs/.config/hypr/scripts/borderless.sh
+++ Configs/.config/hypr/scripts/borderless.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 
-if [ "$BORDERLESS_ENABLED" = "false" ]; then
-    hyprctl reload
-else
-    output=$(hyprctl activeworkspace)
+output=$(hyprctl activeworkspace)
 
-    workspace_id=$(echo $output | grep -o -E '[0-9]+[^0-9]')
+workspace_id=$(echo $output | grep -o -E '[0-9]+[^0-9]')
 
-    IFS=''
-    read -ra ADDR <<<"$workspace_id"
+IFS=''
+read -ra ADDR <<<"$workspace_id"
 
-    for i in "${ADDR[@]}"; do
-        id=$i
-    done
+for i in "${ADDR[@]}"; do
+    id=$i
+done
 
-    echo $id
+echo $id
 
-    hyprctl keyword workspace $id,gapsin:0,gapsout:0,rounding:false,bordersize:0
-fi
+hyprctl keyword workspace $id,gapsin:0,gapsout:0,rounding:false,bordersize:0
